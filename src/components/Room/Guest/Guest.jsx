e16346858238f1b27b34ef80db7297964db394e1
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-05-14 16:21:44 +0900

    feat: 방 참가자 목록 및 참가자 컴포넌트 구현

---
+++ src/components/Room/Guest/Guest.jsx
@@ -0,0 +1,31 @@
+import { styled } from 'styled-components';
+import PropTypes from 'prop-types';
+
+import profileImage from '../../../assets/images/example-profile.svg';
+
+const GuestContainer = styled.li`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  margin-top: 10px;
+  > img {
+    width: 4.5vmin;
+    height: 4.5vmin;
+  }
+`;
+
+function Guest({ name, isOwner }) {
+  return (
+    <GuestContainer>
+      <img src={profileImage} alt="user profile" />
+      {isOwner === true ? <h5>{name}</h5> : <p>{name}</p>}
+    </GuestContainer>
+  );
+}
+
+Guest.propTypes = {
+  name: PropTypes.string.isRequired,
+  isOwner: PropTypes.bool.isRequired,
+};
+
+export default Guest;
