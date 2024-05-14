53c3e60355ed5babc6853768536009eb5b4dbabf
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-05-14 16:21:15 +0900

    feat: 채팅 목록 및 채팅 메세지 컴포넌트 구현

---
+++ src/components/Room/Chat/Message.jsx
@@ -0,0 +1,44 @@
+import { styled } from 'styled-components';
+import PropTypes from 'prop-types';
+
+const MessageContainer = styled.li`
+  width: fit-content;
+  height: 4vh;
+  padding: 5px 10px;
+  background-color: #eff9ff;
+  margin-bottom: 8px;
+  border-radius: 10px;
+  display: flex;
+  position: relative;
+  ${(props) => props.$isMine && 'margin-left: auto; padding: 5px 10px 5px 15px;'}
+`;
+
+const MineMarker = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 8%;
+  height: 100%;
+  border-radius: 10px 0 0 10px;
+  background-color: #008bd4;
+`;
+
+function Message({ user, message, isMine }) {
+  return (
+    <MessageContainer $isMine={isMine}>
+      {isMine && <MineMarker />}
+      <div>
+        <h5>{user}</h5>
+        <p>{message}</p>
+      </div>
+    </MessageContainer>
+  );
+}
+
+Message.propTypes = {
+  user: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+  isMine: PropTypes.bool.isRequired,
+};
+
+export default Message;
