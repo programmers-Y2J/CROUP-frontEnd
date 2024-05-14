0bd9467f535c53b410be6d07e1d99b804ae18d1a
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-05-14 18:51:31 +0900

    feat: 카테고리 버튼 클릭시 변경되는 UI 기능 구현

--- src/components/Room/UserContent.jsx
+++ src/components/Room/UserContent.jsx
@@ -1,6 +1,9 @@
 import { styled } from 'styled-components';
 
+import Question from './Question/Question';
 import Chat from './Chat/Chat';
+
+import useRoomStore from '../../stores/Room/useRoomStore';
 
 const UserContentContainer = styled.div`
   display: flex;
@@ -10,28 +13,68 @@ const UserContentContainer = styled.div`
 
 const MenuTabWrapper = styled.ul`
   display: flex;
+  align-items: center;
   gap: 20px;
-  padding-bottom: 5px;
-  border-bottom: 1px solid #ccc;
   width: 45vw;
+  height: 3vh;
+  border-bottom: 1px solid #ccc;
   > li {
     color: #ccc;
     cursor: pointer;
+    > button {
+      background: none;
+      font-size: 1rem;
+      font-weight: 700;
+      color: #ccc;
+      cursor: pointer;
+    }
+  }
+  > button {
+    padding: 2px 15px;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    border-radius: 50px;
+    color: #ccc;
+    font-size: 0.7rem;
+    font-weight: 500;
+    transition: all 0.5s ease;
+    margin-left: auto;
+    margin-right: 25px;
+    cursor: pointer;
+    &:hover {
+      color: #fff;
+      background-color: #04314d;
+    }
   }
 `;
 
 function UserContent() {
+  const { currentContent, setContent } = useRoomStore();
+
+  const chatButtonStyle = currentContent === 'chat' ? { color: '#04314d' } : {};
+  const questionButtonStyle = currentContent === 'question' ? { color: '#04314d' } : {};
+
+  const handleClickCategory = (content) => {
+    setContent(content);
+  };
+
   return (
     <UserContentContainer>
       <MenuTabWrapper>
         <li>
-          <h4>채팅</h4>
+          <button style={chatButtonStyle} onClick={() => handleClickCategory('chat')} type="button">
+            채팅
+          </button>
         </li>
         <li>
-          <h4>질문 게시판</h4>
+          <button style={questionButtonStyle} onClick={() => handleClickCategory('question')} type="button">
+            질문 게시판
+          </button>
         </li>
+        {currentContent === 'question' && <button type="button">작성</button>}
       </MenuTabWrapper>
-      <Chat />
+      {currentContent === 'chat' && <Chat />}
+      {currentContent === 'question' && <Question />}
     </UserContentContainer>
   );
 }
