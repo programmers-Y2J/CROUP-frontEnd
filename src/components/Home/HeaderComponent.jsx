22060855eb35b3811a50ca7251e125aedb565a4b
namkijoo <rlwn5904@naver.com>
2024-05-16 03:23:25 +0900

    Feat:홈 상단 컴포넌트

---
+++ src/components/Home/HeaderComponent.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { styled } from "styled-components";
+
+function HeaderComponent() {
+  const HeaderContainer = styled.div`
+    margin-left: 15vw;
+    width: 70vw;
+    display: flex;
+    flex-direction: column;
+  `;
+  const HighWrapper = styled.div`
+    display: flex;
+    justify-content: right;
+    margin: 10px 10px;
+    > button {
+      width: 85px;
+      height: 30px;
+      border-radius: 30px;
+      color: white;
+      background-color: #04314d;
+      font-weight: bold;
+      border: 1px solid white;
+      margin-right: 40px;
+    }
+  `;
+  const MiddleWrapper = styled.div`
+    border-radius: 30px;
+    background-color: #00b3ff;
+    height: 40vh;
+    display: flex;
+    > div:nth-child(1) {
+      width: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      > span {
+        font-size: 0.8rem;
+      }
+    }
+    > div:nth-child(2) {
+      display: flex;
+      justify-content: center;
+      align-items: flex-end;
+      width: 50%;
+      writing-mode: vertical-rl; /* 세로로 글씨 쓰기 */
+      text-orientation: sideways;
+      transform: rotate(180deg);
+      font-size: 5rem;
+      font-weight: 700;
+      padding-left: 40px;
+      color: white;
+    }
+  `;
+  const LowWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 25vh;
+  `;
+  return (
+    <HeaderContainer>
+      <HighWrapper>
+        <button>Login</button>
+      </HighWrapper>
+      <MiddleWrapper>
+        <div>
+          <p>
+            <h1>
+              집중을 위한 공간을 만나는 <br />
+              최고의 방법
+            </h1>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+          </p>
+        </div>
+        <div>Croup</div>
+      </MiddleWrapper>
+      <LowWrapper>
+        <h2>오늘 하루 즐거운 집중을 찾아보세요.</h2>
+      </LowWrapper>
+    </HeaderContainer>
+  );
+}
+
+export default HeaderComponent;
