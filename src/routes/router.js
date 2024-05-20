77854c3de31286368ce9404565fb83452a11d16f
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-05-20 22:59:10 +0900

    feat: router에 Home 컴포넌트 추가

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Room from '../pages/Room';
+import Home from '../pages/Home';
 
 const router = createBrowserRouter([
-  // {
-  //   path: '/',
-  //   element: <Home />,
-  // },
+  {
+    path: '/',
+    element: <Home />,
+  },
   {
     path: 'room/:roomId',
     element: <Room />,
