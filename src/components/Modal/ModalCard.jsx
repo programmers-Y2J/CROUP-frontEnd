8fdefb5d72a66b5435663723d5a6d5d9313fedea
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-05-20 13:39:25 +0900

    feat: ModalCard 컴포넌트 UI 구현

---
+++ src/components/Modal/ModalCard.jsx
@@ -0,0 +1,54 @@
+import { styled } from 'styled-components';
+
+import closeIcon from '../../assets/icons/close-icon.svg';
+
+import PostDetail from './PostDetail';
+
+const ModalContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledModalCard = styled.div`
+  display: inline-block;
+  padding: 80px 50px;
+  background-color: #fff;
+  position: relative;
+  border-radius: 20px;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 18px;
+  right: 3%;
+  background: none;
+  cursor: pointer;
+  img {
+    width: 15px;
+    height: 15px;
+  }
+`;
+
+function ModalCard() {
+  return (
+    <ModalContainer>
+      <StyledModalCard>
+        <PostDetail />
+        <CloseButton>
+          <img src={closeIcon} alt="close-button" />
+        </CloseButton>
+      </StyledModalCard>
+    </ModalContainer>
+  );
+}
+
+export default ModalCard;
