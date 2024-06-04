e4c3756d8729f9f79abf6c00995b708f4020696c
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-06-04 17:49:45 +0900

    feat: global theme 구현

---
+++ src/styles/theme.js
@@ -0,0 +1,48 @@
+const fontSize = {
+  title: 50,
+  subTitle: 25,
+  xsmall: 8,
+  small: 10,
+  medium: 13,
+  large: 15,
+  xlarge: 20,
+  xxlarge: 35,
+};
+
+const fontWeight = {
+  extraBold: 800,
+  bold: 700,
+  semiBold: 600,
+  medium: 500,
+  regular: 400,
+};
+
+const spacing = {
+  section: 50,
+  chat: 5,
+  small: 7,
+  medium: 15,
+  large: 30,
+};
+
+const color = {
+  50: '##eff9ff',
+  100: '#def2ff',
+  200: '#b6e8ff',
+  primary: '#00B3FF',
+  white: '#fefefe',
+  black: '#000000',
+  gray: '#9d9d9d',
+  background: '#ffffff',
+  border: '#f1f1f1',
+  placehorder: '#ccc',
+};
+
+const theme = {
+  fontSize,
+  spacing,
+  color,
+  fontWeight,
+};
+
+export default theme;
