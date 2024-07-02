07afea875454600086db16027e01c9387cc13117
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-07-02 20:46:14 +0900

    remove: 사용하지 않는 파일 삭제

--- src/components/Modal/ModalPortal.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDom from 'react-dom';
-
-function ModalPortal({ children }) {
-  const modalRoot = document.getElementById('modalRoot');
-  return ReactDom.createPortal(children, modalRoot);
-}
-
-export default ModalPortal;
