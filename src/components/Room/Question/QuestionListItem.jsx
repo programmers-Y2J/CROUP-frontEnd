11291a1d7f4dbd16ea4adbd734fa833b09634177
namkijoo <rlwn5904@naver.com>
2024-07-08 16:09:20 +0900

    merge

--- src/components/Room/Question/QuestionListItem.jsx
+++ src/components/Room/Question/QuestionListItem.jsx
@@ -1,7 +1,7 @@
 import { Card, CardContent } from '@ui/card';
 import { Badge } from '@ui/badge';
 import { useNavigate } from 'react-router-dom';
-import { useQuestionStore } from '@/stores/Room/useRoomStore';
+import { useQuestionStore } from '@/stores/Room/useAuthStore';
 
 function QuestionListItem({ questionId, title, nickName, content, setActiveTab }) {
   const navigate = useNavigate();
